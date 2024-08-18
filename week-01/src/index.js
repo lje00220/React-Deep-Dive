30929f8bd3194770742a9f42ee5c8baa2c8d0f82
lje00220 <155710708+lje00220@users.noreply.github.com>
2024-08-18 15:03:18 +0900

    계산기 수정, md파일 추가

--- week-01/src/index.js
+++ week-01/src/index.js
@@ -6,6 +6,11 @@ const addButton = document.getElementById("add");
 const subButton = document.getElementById("substract");
 const mulButton = document.getElementById("multiply");
 const divButton = document.getElementById("divide");
+// function checkType(a: unknown, b: unknown) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     alert("잘못된 입력입니다. 숫자를 입력해주십시오.");
+//   }
+// }
 function add(a, b) {
     return a + b;
 }
@@ -16,17 +21,29 @@ function multiply(a, b) {
     return a * b;
 }
 function divide(a, b) {
+    if (b === 0) {
+        throw new Error("0이 아닌 다른 숫자를 입력해주세요.");
+    }
     return a / b;
 }
 addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener("click", () => {
+    // checkType(num1, num2);
     result.textContent = add(Number(num1.value), Number(num2.value)).toString();
 });
 subButton === null || subButton === void 0 ? void 0 : subButton.addEventListener("click", () => {
+    // checkType(num1, num2);
     result.textContent = substract(Number(num1.value), Number(num2.value)).toString();
 });
 mulButton === null || mulButton === void 0 ? void 0 : mulButton.addEventListener("click", () => {
+    // checkType(num1, num2);
     result.textContent = multiply(Number(num1.value), Number(num2.value)).toString();
 });
 divButton === null || divButton === void 0 ? void 0 : divButton.addEventListener("click", () => {
-    result.textContent = divide(Number(num1.value), Number(num2.value)).toString();
+    // checkType(num1, num2);
+    try {
+        result.textContent = divide(Number(num1.value), Number(num2.value)).toString();
+    }
+    catch (e) {
+        // result.textContent = e.message;
+    }
 });
--- week-01/src/index.ts
+++ week-01/src/index.ts
@@ -7,6 +7,12 @@ const subButton = document.getElementById("substract");
 const mulButton = document.getElementById("multiply");
 const divButton = document.getElementById("divide");
 
+// function checkType(a: unknown, b: unknown) {
+//   if (typeof a !== "number" || typeof b !== "number") {
+//     alert("잘못된 입력입니다. 숫자를 입력해주십시오.");
+//   }
+// }
+
 function add(a: number, b: number): number {
   return a + b;
 }
@@ -20,27 +26,38 @@ function multiply(a: number, b: number): number {
 }
 
 function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error("0이 아닌 다른 숫자를 입력해주세요.");
+  }
   return a / b;
 }
 
 addButton?.addEventListener("click", () => {
+  // checkType(num1, num2);
   result.textContent = add(Number(num1.value), Number(num2.value)).toString();
 });
 subButton?.addEventListener("click", () => {
+  // checkType(num1, num2);
   result.textContent = substract(
     Number(num1.value),
     Number(num2.value)
   ).toString();
 });
 mulButton?.addEventListener("click", () => {
+  // checkType(num1, num2);
   result.textContent = multiply(
     Number(num1.value),
     Number(num2.value)
   ).toString();
 });
 divButton?.addEventListener("click", () => {
-  result.textContent = divide(
-    Number(num1.value),
-    Number(num2.value)
-  ).toString();
+  // checkType(num1, num2);
+  try {
+    result.textContent = divide(
+      Number(num1.value),
+      Number(num2.value)
+    ).toString();
+  } catch (e) {
+    // result.textContent = e.message;
+  }
 });
